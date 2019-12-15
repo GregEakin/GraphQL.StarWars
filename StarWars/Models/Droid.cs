582795e8e7742bc22593ddfd5683500f3acc37c7
Greg Eakin <gregory.eakin@gmail.com>
2019-12-15 07:59:26 -0800

    Update file header with Apache License

--- StarWars/Models/Droid.cs
+++ StarWars/Models/Droid.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections.Generic;
+﻿// Copyright 2019 Greg Eakin
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
 
 namespace StarWars.Models
 {
     public class Droid
        : ICharacter
     {
+        public string PrimaryFunction { get; set; }
+
+        public bool Restrained { get; } = false;
         public string Id { get; set; }
 
         public string Name { get; set; }
@@ -13,10 +29,6 @@ namespace StarWars.Models
 
         public IReadOnlyList<Episode> AppearsIn { get; set; }
 
-        public string PrimaryFunction { get; set; }
-
         public double Height { get; } = 0.72;
-
-        public bool Restrained { get; } = false;
     }
 }
